abc9df1143266080460d9143bc39352e045a69aa
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-05-29 09:24:02 +0100

    build(nextconfig): re-added experimental flag for nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   after: true,
-  //   ppr: 'incremental',
-  //   reactCompiler: {
-  //     compilationMode: 'annotation'
-  //   }
-  // }
+  experimental: {
+    // after: true,
+    ppr: "incremental",
+    reactCompiler: {
+      compilationMode: "annotation",
+    },
+  },
 };
 
 export default nextConfig;
