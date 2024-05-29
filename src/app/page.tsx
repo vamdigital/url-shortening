0aa52470553e51d0dd6b723ff74016b8aca95178
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-05-29 10:33:08 +0100

    style(tailwindconfig): add custom font and container sizing breakpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="container">
-      <h1 className="text-4xl font-bold">Shortly</h1>
+    <div className='container'>
+      <h1 className='text-4xl font-bold'>Shortly</h1>
     </div>
-  );
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,36 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       container: {
         center: true,
         screens: {
-          sm: "375px",
-          md: "728px",
-          lg: "984px",
-          xl: "1240px",
-          "2xl": "1440px",
-        },
+          sm: '375px',
+          md: '728px',
+          lg: '984px',
+          xl: '1240px',
+          '2xl': '1440px'
+        }
       },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
       },
       fontFamily: {
         // sans: ['var(--font-geist-sans)'],
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans]
         // mono: ['var(--font-geist-mono)']
-      },
-    },
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
