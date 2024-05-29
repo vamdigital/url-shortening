1c82043412eb1fd85a6190894e3c394165c0bb55
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-05-29 10:55:33 +0100

    feat(header component): initial commit for Header Component

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,13 +1,18 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export const Header = () => {
   return (
-    <header className="flex w-full py-10">
-      <div className="container flex">
-        <div className="flex">
-          <Image src="/logo.svg" alt="Shortly logo" width={150} height={50} />
+    <header className='flex w-full py-10'>
+      <div className='container flex'>
+        <div className='flex'>
+          <Image
+            src='/logo.svg'
+            alt='Shortly logo'
+            width={150}
+            height={50}
+          />
         </div>
       </div>
     </header>
-  );
-};
+  )
+}
